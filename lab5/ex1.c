5695959f2e4eafb55495fcdd9d93f1c6a8e196c0
Bernardo-PUC <bernardo.vs245@gmail.com>
2025-09-23 12:12:14 -0300

    exericio 1

---
+++ lab5/ex1.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+int main(int argc, char *argv[])
+{
+	int pipefd[2];
+	pid_t pid;
+	char buffer[100];
+	int bytes_read;
+
+	if (pipe(pipefd) == -1) {
+		perror("pipe");
+		return 1;
+	}
+
+	pid = fork();
+	if (pid == -1) {
+		perror("fork");
+		return 1;
+	}
+
+	if (pid == 0)
+	{
+		close(pipefd[0]);
+		write(pipefd[1], "Hello World!\n", 13);
+		close(pipefd[1]);
+		return 0;
+	}
+
+	close(pipefd[1]);
+	bytes_read = read(pipefd[0], buffer, 100);
+	printf("Bytes read: %d\n", bytes_read);
+	printf("Buffer: %s\n", buffer);
+	close(pipefd[0]);
+
+	return 0;
+}
